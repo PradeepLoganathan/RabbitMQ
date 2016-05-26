38f7c73525339963cd19284696c2751c637cade6
Pradeep Loganathan <pradeep@pradeeploganathan.com>
2016-05-26 19:25:02 +0530

    Adding files

---
+++ BasicQueue/Sender/SenderProgram.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Sender
+{
+    class SenderProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Started Sender....");
+            
+            Producer producer = new Producer();
+            producer.Initialize();
+            for(int i=0;i<100;i++)
+                producer.SendMessage();
+            producer.ShutDown();
+            Console.ReadLine();
+
+        }
+    }
+
+
+    class Producer
+    {
+
+        private ConnectionFactory factory;
+        private IConnection connection;
+        private IModel channel;
+        private static string QIdentifier;
+        private static int Qid;
+
+        public Producer()
+        {
+            QIdentifier = " Noise Generator - ";
+            Qid = DateTime.Now.Second;            
+                
+        }
+
+        public bool Initialize()
+        {
+            try
+            {
+                factory = new ConnectionFactory();
+                factory.HostName = "localhost";
+                factory.UserName = "guest";
+                factory.Password = "guest";
+
+                connection = factory.CreateConnection();
+                Console.WriteLine("Created Connection....");
+                channel = connection.CreateModel();
+                Console.WriteLine("Created Channel....");
+                channel.QueueDeclare("MyQ", false, false, false, null);
+                Console.WriteLine("Created Queue..." + QIdentifier + Qid.ToString());
+
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool SendMessage()
+        {
+            try
+            {
+                Random r;
+                r = new Random();
+
+                string message = "Message" + r.Next().ToString();
+                byte[] body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("", "MyQ", null, body);
+                Console.WriteLine("Published Message - " + message);
+                
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        internal void ShutDown()
+        {
+            if(connection.IsOpen)
+                connection.Close();
+            
+            if(channel.IsOpen)
+                channel.Close();
+
+            connection.Dispose();
+            channel.Dispose();
+
+            channel = null;
+            connection = null;
+        }
+    }
+
+}
---
+++ PubSub/Subscriber/SubscribeProgram.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Subscriber
+{
+    class SubscriberProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Started Subscriber...");
+            Subscriber s = new Subscriber();
+            s.Initialize();
+            s.Subscribe();
+            s.Shutdown();
+        }
+    }
+
+    class Subscriber
+     {
+
+         private ConnectionFactory Factory;
+         private IConnection Connection;
+         private IModel Channel;
+         private string QueueName;
+         private EventingBasicConsumer Consumer;
+
+
+         public void Initialize()
+         {
+             try
+             {
+                 Console.WriteLine("Subscriber Initializing..");
+                 Factory = new ConnectionFactory();
+                 Factory.HostName = "localhost";
+                 Factory.UserName = "guest";
+                 Factory.Password = "guest";
+                 Connection = Factory.CreateConnection();
+                 Channel = Connection.CreateModel();
+                 Channel.ExchangeDeclare("MyExchange", "fanout");
+                 Consumer = new EventingBasicConsumer(Channel);
+                 Consumer.Received += Consumer_Received;
+                 Console.WriteLine("Subscriber Intialized");
+             }
+             catch (Exception)
+             {
+
+                 throw;
+             }  
+         }
+
+         public void Subscribe()
+         {
+            // BasicDeliverEventArgs result;
+
+             try
+             {
+                 QueueName = Channel.QueueDeclare().QueueName;
+                 Console.WriteLine("Queue Name is " + QueueName);
+                 Channel.QueueBind(QueueName, "MyExchange", "");
+                 Console.WriteLine("Consumer waiting for Messages...");
+
+                 while (true)
+                 {
+                     Channel.BasicConsume(QueueName, true, Consumer);
+                     Console.WriteLine("Waiting on subscription....");
+                     Console.ReadLine();
+                     break;
+                 }
+
+                 //for (int i = 0; i < 100; i++)
+                 //{
+                 //    Channel.BasicConsume(QueueName, true, Consumer);
+                 //    System.Threading.Thread.Sleep(400);
+                 //}
+
+                 Console.WriteLine("Subscriber Subscribed");
+             }
+             catch (Exception e)
+             {
+                 Console.WriteLine(e.Message.ToString());
+                 throw;
+             }
+         }
+
+         private void Consumer_Received(object sender, BasicDeliverEventArgs e)
+         {
+             Console.WriteLine("Subscriber reieved Message");
+             Console.WriteLine(Encoding.UTF8.GetString(e.Body));
+
+         }
+
+         public void Shutdown()
+         {
+             Channel.Close();
+             Channel.Dispose();
+
+             Connection.Close();
+             Connection.Dispose();
+
+             Console.ReadLine();
+         }
+     }
+    
+}
